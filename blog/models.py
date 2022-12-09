31bc6a6831035c3f4c752917c4b5ddd2cdbfac61
Kolihay <tpro83840@gmail.com>
2022-12-09 15:34:50 +0200

    Main

--- blog/models.py
+++ blog/models.py
@@ -4,4 +4,8 @@ from django.db import models
 
 class Post(models.Models):
     title = models.CharField(max_length=200, unique=True)
-    
+    content = models.TextField()
+    short_description = models.CharField(max_length=100, blank=True, null=True)
+    likes_of_amount = models.CharField(max_length=3, choices=True)
+    author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+    creation_date = models.DateTimeField(auto_now=True)
