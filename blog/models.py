c1c2383ae0240cb68e6f53045319de8e5375b891
Kolihay <tpro83840@gmail.com>
2022-12-09 15:08:48 +0200

    'mm'

--- blog/models.py
+++ blog/models.py
@@ -3,4 +3,5 @@ from django.db import models
 # Create your models here.
 
 class Post(models.Models):
-    title = models.CharField(max_length=200, )
+    title = models.CharField(max_length=200, unique=True)
+    
